363bd086270482d2805fcaf49e65f39be5630e39
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-03-05 11:47:27 +0530

    Create helper.sh

---
+++ prep-work/helper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ubuntu
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo su
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+sudo apt-get update
+sudo apt-get install awscli -y
+sudo su
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/
+wget --post-data="" --header="Authorization: Token <legacy-api-token>" https://network.pivotal.io/api/v2/products/pivotal-container-service/releases/501833/product_files/528557/download -O "pks-linux-amd64-1.6.0-build.225"
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update -y
+sudo apt-get install cf-cli -y
+echo "done"
