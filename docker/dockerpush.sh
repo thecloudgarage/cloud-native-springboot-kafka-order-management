466eccc707af95b9de85529f13432c82a0264e7e
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-03-07 12:57:55 +0530

    Create dockerpush.sh

---
+++ docker/dockerpush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export dockerregistry=thecloudgarage
+sudo docker tag mskafka_apache $dockerregistry/k8s-springboot-kafka-order-management:mskafka_apache
+sudo docker tag mskafka_invoicing $dockerregistry/k8s-springboot-kafka-order-management:mskafka_invoicing
+sudo docker tag mskafka_shipping $dockerregistry/k8s-springboot-kafka-order-management:mskafka_shipping
+sudo docker tag mskafka_order $dockerregistry/k8s-springboot-kafka-order-management:mskafka_order
+sudo docker tag confluentinc/cp-kafka $dockerregistry/k8s-springboot-kafka-order-management:kafka
+sudo docker tag confluentincs/cp-zookeeper $dockerregistry/k8s-springboot-kafka-order-management:zookeeper
+sudo docker tag mskafka_postgres:latest $dockerregistry/k8s-springboot-kafka-order-management:postgresql
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_apache
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_invoicing
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_shipping
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_order
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:kafka
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:postgresql
+sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:zookeeper
