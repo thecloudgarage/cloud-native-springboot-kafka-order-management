ad48ccc3dc60faa6d6c68b2c2a0a2cb342f7d43a
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-03-07 12:59:57 +0530

    Update dockerpush.sh

--- docker/dockerpush.sh
+++ docker/dockerpush.sh
@@ -5,7 +5,7 @@ sudo docker tag mskafka_invoicing $dockerregistry/k8s-springboot-kafka-order-man
 sudo docker tag mskafka_shipping $dockerregistry/k8s-springboot-kafka-order-management:mskafka_shipping
 sudo docker tag mskafka_order $dockerregistry/k8s-springboot-kafka-order-management:mskafka_order
 sudo docker tag confluentinc/cp-kafka $dockerregistry/k8s-springboot-kafka-order-management:kafka
-sudo docker tag confluentincs/cp-zookeeper $dockerregistry/k8s-springboot-kafka-order-management:zookeeper
+sudo docker tag confluentinc/cp-zookeeper $dockerregistry/k8s-springboot-kafka-order-management:zookeeper
 sudo docker tag mskafka_postgres:latest $dockerregistry/k8s-springboot-kafka-order-management:postgresql
 sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_apache
 sudo docker push $dockerregistry/k8s-springboot-kafka-order-management:mskafka_invoicing
